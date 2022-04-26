353fb94585429ca95a125ed88d96980bb8be7210
nipungoel2000 <nipungoel350@gmail.com>
2022-04-26 08:21:43 +0530

    Download CSV button and fixed sort by

--- client/src/components/AdminViewBookings.js
+++ client/src/components/AdminViewBookings.js
@@ -1,17 +1,19 @@
 import React, { useState, useContext, createContext, useEffect}  from "react";
-import 'bootstrap/dist/css/bootstrap.css';
 import {Link, useNavigate} from 'react-router-dom';
 // import { tokenContext } from '../App';
-import './test.css'
+// import './test.css'
 import {Form,Button,Row,Col,Container} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+// import "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
 import NavigationBar from './AdminNavigationBar';
-import fileDownload from 'js-file-download'
+// import fileDownload from 'js-file-download'
+import {CSVDownload,CSVLink} from "react-csv";
 import axios from "axios";
 
 function AdminViewBookings(){
     const navigate = useNavigate();
     const [bookings, setBookings] = useState([]);
-    const [sortby, setsortby] = useState("RoomNumber");
+    const [sortby, setsortby] = useState("default");
 
     useEffect(() => {
         console.log("here");
@@ -24,23 +26,18 @@ function AdminViewBookings(){
     });
 
     useEffect(()=>{
-
+      // if(sortby=="default")
+      //   alert("Please choose the sorting criteria in result");
+      if(sortby!="default"){
         axios({
             url: "http://localhost:3001/booking/fetchall",
             method: "POST",
             data: {sortby:sortby},
         }).then((res) => {
-            // console.log("yot list",res.data.bookingData);
+            
             if(res.status==201){
-                if(res.data.bookingsLst==0)
-                {  
-                    console.log("no data");
-                }
-                else
-                {
-                    setBookings(res.data.bookingsLst);
-                    console.log("data");                
-                }
+              console.log(res.data.bookingsLst);
+              setBookings(res.data.bookingsLst);
             }
             else{
                 alert("Internal Server Error");
@@ -50,73 +47,47 @@ function AdminViewBookings(){
             console.log(err);
             // console.log(res.status);
             alert("Internal Server Error ");
-        })
+        }) }
     },[sortby]);
-
-    async function download(e){
-      e.preventDefault();
-      // fileDownload([[1,2,3],[4,5,6]], "bookings.csv");
-      //   axios({
-      //     url: "http://localhost:3001/booking/fetchall",
-      //     method: "POST",
-      //     data: {sortby:sortby},
-      // }).then((res) => {
-      //     // console.log("yot list",res.data.bookingData);
-      //     if(res.status==201){
-      //         if(res.data.bookingsLst==0)
-      //         {  
-      //             console.log("no data");
-      //         }
-      //         else
-      //         {
-      //             fileDownload(res.data.bookingsLst, "bookings.csv");
-      //             console.log("data");                
-      //         }
-      //     }
-      //     else{
-      //         alert("Internal Server Error");
-      //     }
-      // })
-      // .catch((err) => {
-      //     console.log(err);
-      //     // console.log(res.status);
-      //     alert("Internal Server Error ");
-      // })
-    }
 
     return(
         <>
         <NavigationBar/>
+        {/* <div>
+          <Row>
+            <Col>Test1</Col>
+            <Col> Test2</Col>
+          </Row>
+        </div> */}
         <div>
           <div className="d-flex justify-content-around">
-            <Form.Group className="p-2 col-example text-left">
-                {/* <Form.Label>Hello</Form.Label> */}
-                <Form.Select onChange={event => {
+            <Form>
+            <Form.Group className="p-2 col-example text-left form-inline">
+                {/* <Form.Label for>Hello</Form.Label> */}
+                <Form.Select defaultValue={sortby} onChange={event => {
                     console.log(event.target.value);
                     setsortby(event.target.value);
                 }}>
-                    <option disabled>Sort by</option>
+                    <option value="default" disabled hidden>Sort by</option>
                     <option value="RoomNumber">Room Number</option>
                     <option value="Time">Time</option>
                 </Form.Select>
             </Form.Group>
-      <Button className="mt-2 mb-2 col-example text-left" variant="primary" onClick={download}>Download</Button>
-      </div>
-            {/* <div>
-            <Button className="d-flex flex-row-reverse" variant="primary">Download</Button>
-            </div> */}
-            <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                <th scope="col">Sr.No</th>
-                <th scope="col">Booking ID</th>
-                <th scope="col">Email</th>
-                <th scope="col">Room Number</th>
-                <th scope="col">Date</th>
-                <th scope="col">Time</th>
-                </tr>
-            </thead>
-
+            </Form>
+      {/* <Button className="mt-2 mb-2 col-example text-left" variant="primary" onClick={download}>Download</Button> */}
+          <CSVLink data={bookings} filename={"Bookings.csv"} className="btn btn-primary mt-2 mb-2 col-example text-left" target="_blank"> Download All</CSVLink>
+          </div>
+          <table className="table">
+          <thead className="thead-dark">
+              <tr>
+              <th scope="col">Sr.No</th>
+              <th scope="col">Booking ID</th>
+              <th scope="col">Email</th>
+              <th scope="col">Room Number</th>
+              <th scope="col">Date</th>
+              <th scope="col">Time</th>
+              </tr>
+          </thead>
           {bookings.map((booking, index) => (
             <>
               <tbody style={{backgroundColor:' rgba(0, 0, 0, 0.637)'}}>
